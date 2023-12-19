010f5b6dccc7a95c28f847c6144f15c2f44d244b
Sakib Hasan <sakib100.sa@gmail.com>
2023-12-19 15:13:20 +0600

    project complete

--- JS Projects/Scroll-Spy/script.js
+++ JS Projects/Scroll-Spy/script.js
@@ -11,7 +11,9 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             links.forEach((link) => {
                 link.classList.remove("active");
-                document.querySelector("header nav a[href*=" + id + "]");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
             });
         }
     });

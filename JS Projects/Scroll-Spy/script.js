9ba65c66eb4009e5783a9b4e7c205558461d6d4e
Sakib Hasan <sakib100.sa@gmail.com>
2023-12-19 11:30:02 +0600

    tracking the scroll

--- JS Projects/Scroll-Spy/script.js
+++ JS Projects/Scroll-Spy/script.js
@@ -1,1 +1,18 @@
-console.log("console");
+let sections = document.querySelectorAll("section");
+let links = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach((section) => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            links.forEach((link) => {
+                link.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]");
+            });
+        }
+    });
+};

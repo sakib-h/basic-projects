4a6d74a4b79e5eb6185990425eb211dd060a4b7e
Sakib Hasan <sakib100.sa@gmail.com>
2023-12-21 02:51:00 +0600

    animation complete

--- JS Projects/Sidebar-Menu/script.js
+++ JS Projects/Sidebar-Menu/script.js
@@ -1,7 +1,17 @@
 let menuToggle = document.querySelector(".menuToggle");
 let sidebar = document.querySelector(".sidebar");
+let menuLists = document.querySelectorAll(".menuList li");
 
 menuToggle.onclick = function () {
     menuToggle.classList.toggle("active");
     sidebar.classList.toggle("active");
 };
+
+function activeLink() {
+    menuLists.forEach((list) => {
+        list.classList.remove("active");
+        this.classList.add("active");
+    });
+}
+
+menuLists.forEach((list) => list.addEventListener("click", activeLink));
